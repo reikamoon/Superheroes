5df226dc5961b9737fc6a221d952f1825d181815
Anjelica <54198467+reikamoon@users.noreply.github.com>
2019-09-27 11:25:42 -0700

    added team class.

--- superheroes_assignment.py
+++ superheroes_assignment.py
@@ -29,8 +29,20 @@ class Armor:
         self.defense_value = random.randint(0,500)
         self.max_block = 500
         return self.defense_value
+
+class Weapon(Ability):
+    def __init__(self, name, attack_value):
+        super().__init__(name, attack_value)
+    def attack(self):
+        """  This method returns a random value
+        between one half to the full attack power of the weapon.
+        """
+        # TODO: Use what you learned to complete this method.
+        damage = random.randint(0, self.attack_value)
+        return damage // 2
+
 class Hero:
-    def __init__(self, name, starting_health = 5000, defense_value = 50):
+    def __init__(self, name, deaths = 0, kills = 0, starting_health = 5000, defense_value = 50, attack_value = 50):
         '''Instance properties:
         abilities: List
         armors: List
@@ -41,8 +53,11 @@ class Hero:
         # TODO: Initialize instance variables values as instance variables
         self.abilities = []
         self.armor = []
+        self.deaths = 0
+        self.kills = 0
         self.name = name
         self.defense_value = defense_value
+        self.attack_value = attack_value
         self.starting_health = self.current_health = starting_health
         # (Some of these values are passed in above,
         # others will need to be set at a starting value)
@@ -58,6 +73,14 @@ class Hero:
          '''
          # TODO: Add armor object that is passed in to `self.armors`
          self.armor.append(armor)
+
+    def add_kills(self, num_kills):
+        ''' Update kills with num_kills'''
+        # TODO: This method should add the number of kills to self.kills
+        self.kills += num_kills
+
+    def add_deaths(self, num_deaths):
+        self.deaths = num_deaths
 
     def attack(self):
         '''Calculate the total damage from all ability attacks.
@@ -126,6 +149,8 @@ class Hero:
         if self.is_alive() == False:
             print("{0} has fallen!".format(self.name))
             print("{0} wins!".format(opponent.name))
+            self.add_deaths(1)
+            opponent.add_kills(1)
             if opponent.name == "Double" and opponent.is_alive() == True:
                 print("Phillip: Shotaro, now would be the time to make a strategic retreat.")
                 print("Shotaro: Fine. But I won't forget this!")
@@ -136,33 +161,90 @@ class Hero:
         if opponent.is_alive() == False:
             print("{0} has fallen!".format(opponent.name))
             print("{0} wins!".format(self.name))
+            opponent.add_deaths(1)
+            self.add_kills(1)
             if self.name == "Double" and self.is_alive() == True:
                 print("Phillip: Shotaro, now would be the time to make a strategic retreat.")
                 print("Shotaro: Fine. But I won't forget this!")
             if self.name == "Kiva" and self.is_alive() == True:
                 print("Kivat: Wataru, we need to retreat!")
                 print("Wataru: Too strong...")
+        #TODO: Refactor this method to update the
+        # number of kills the hero has when the opponent dies.
+        # Also update the number of deaths for whoever dies in the fight
+class Team:
+    def __init__(self, teamname):
+        ''' Initialize your team with its team name
+        '''
+    # TODO: Implement this constructor by assigning the name and heroes, which should be an empty list
+        self.heroes = []
+        self.teamname = teamname
+    def attack(self, other_team):
+         # TODO: Randomly select a living hero from each team and have
+        # them fight until one or both teams have no surviving heroes.
+        # Hint: Use the fight method in the Hero class.
+    def remove_hero(self, name):
+        '''Remove hero from heroes list.
+        If Hero isn't found return 0.
+        '''
+        # TODO: Implement this method to remove the hero from the list given their name.
+        for hero in self.heroes:
+            if name == hero.name:
+                self.heroes.remove(hero)
+                return 1
+        return 0
 
+    def add_hero(self, hero):
+          '''Add Hero object to self.heroes.'''
+          # TODO: Add the Hero object that is passed in to the list of heroes in
+          # self.heroes
+          self.heroes.append(hero)
+    def view_all_heroes(self):
+         '''Prints out all heroes to the console.'''
+        # TODO: Loop over the list of heroes and print their names to the terminal.
+        for hero in self.heroes:
+            print("{0}".format(hero.name))
+    def revive_heroes(self, health = 5000):
+        ''' Reset all heroes health to starting_health'''
+        # TODO: This method should reset all heroes health to their
+        # original starting value.
+
+    def stats(self):
+        '''Print team statistics'''
+        # TODO: This method should print the ratio of kills/deaths for each
+        # member of the team to the screen.
+        # This data must be output to the console.
+        # Hint: Use the information stored in each hero.
+        
 if __name__ == "__main__":
     # If you run this file from the terminal
     # this block is executed.
-    my_hero = Hero("Double")
     ability = Ability("Double Boiled Extreme", 350)
     ability2 = Ability("Flame Maximum Drive", 200)
+    ability3 = Ability("Wake up Fever!", 350)
+    ability4 = Ability("Break the Chain", 200)
+    ability5 = Ability("Clock up", 200)
+    ability6 = Ability("Hyper Kick", 350)
     armor = Armor("Cyclone", 200)
     armor2 = Armor("Joker", 200)
+    armor3 = Armor("Emperor's Garb", 200)
+    shield = Armor("Emperor's Shield", 200)
+    armor4 = Armor("ZECT Kabuto Armor", 200)
+    armor5 = Armor("ZECT Kabuto Cast", 200)
+    my_Team = Team("Rider War")
+    my_hero = Hero("Double")
     my_hero.add_armor(armor)
     my_hero.add_armor(armor2)
     my_hero.add_ability(ability)
     my_hero.add_ability(ability2)
     my_hero2 = Hero("Kiva")
-    ability3 = Ability("Wake up Fever!", 350)
-    ability4 = Ability("Break the Chain", 200)
-    armor3 = Armor("Emperor's Garb", 200)
-    shield = Armor("Emperor's Shield", 200)
     my_hero2.add_armor(armor3)
     my_hero2.add_armor(shield)
     my_hero2.add_ability(ability3)
     my_hero2.add_ability(ability4)
+    my_hero3 = Hero("Kabuto")
+    my_hero3.add_armor(armor4)
+    my_hero3.add_armor(armor5)
     #my_hero.begin_fight()
     my_hero.fight(my_hero2)
+    print(my_hero.kills)
